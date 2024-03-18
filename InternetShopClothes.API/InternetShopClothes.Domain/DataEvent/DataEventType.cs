62225e1e92b9469a12a1e6e29c32f9b5087bdbb8
zhangir003 <zhangir003@mail.ru>
2024-03-18 16:32:41 +0600

    Add Data Event Type

---
+++ InternetShopClothes.API/InternetShopClothes.Domain/DataEvent/DataEventType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShopClothes.Domain.DataEvent
+{
+    public enum DataEventType
+    {
+        Added = 1,
+        Update,
+        Deleted,
+    }
+}
