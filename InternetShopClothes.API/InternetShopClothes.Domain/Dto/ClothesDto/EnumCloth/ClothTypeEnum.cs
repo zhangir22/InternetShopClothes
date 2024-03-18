adcd535d2bb2ff5fbbfbf517e0cd47bb4c994847
zhangir003 <zhangir003@mail.ru>
2024-03-18 16:30:04 +0600

    Add Cloth Type Helper

---
+++ InternetShopClothes.API/InternetShopClothes.Domain/Dto/ClothesDto/EnumCloth/ClothTypeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShopClothes.Domain.Dto.ClothesDto.EnumCloth
+{
+    public enum ClothTypeEnum
+    {
+        Sport,
+        Classic,
+        Casual,
+        Work,
+    }
+}
---
+++ InternetShopClothes.API/InternetShopClothes.Domain/Helpers/ClothTypeHelper.cs
@@ -0,0 +1,41 @@
+﻿using InternetShopClothes.Domain.Dto.ClothesDto.EnumCloth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShopClothes.Domain.Helpers
+{
+    public class ClothTypeHelper
+    {
+        public static string GetClothType(ClothTypeEnum type)
+        {
+            switch (type)
+            {
+                case ClothTypeEnum.Casual:
+                    return "Повседневный";
+                case ClothTypeEnum.Sport:
+                    return "Спортивный";
+                case ClothTypeEnum.Classic:
+                    return "Классический";
+                case ClothTypeEnum.Work:
+                    return "Рабочая одежда";
+                default:
+                    return string.Empty;
+            }
+        }
+        public static string GetGenderCloth(ClothGenderEnum gender) 
+        {
+            switch(gender)
+            {
+                case ClothGenderEnum.Male:
+                    return "Мужчина";
+                case ClothGenderEnum.Female:
+                    return "Женщина";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
