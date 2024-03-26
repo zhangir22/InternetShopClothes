e60f0a44c97913ee139a973b95276daa2ddfaddc
zhangir003 <zhangir003@mail.ru>
2024-03-26 09:44:22 +0600

    Big Update

---
+++ InternetShopClothes.API/InternetShopClothes.Infrastructure/InternetShopClothDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShopClothes.Infrastructure
+{
+    public class InternetShopClothDbContext:DbContext
+    {
+        public InternetShopClothDbContext(DbContextOptions options) : base(options) { }
+    }
+}
---
+++ InternetShopClothes.API/InternetShopClothes.Infrastructure/Models/Cloth/ClothModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShopClothes.Infrastructure.Models.Cloth
+{
+    public class ClothModel:BaseModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Type { get; set; }
+        public string? Textile { get; set; }
+        public string? TextileColor { get; set; }
+        public int? GenderCloth { get; set; }
+        public int? TypeCloth { get; set; }
+    }
+}
